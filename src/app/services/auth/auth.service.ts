3f75a1cba51cb7bb09c498e2f01683d43ec6ff6e
Burence1 <burensomondi@gmail.com>
2021-08-14 15:01:13 +0300

    add email signup, password reset and logout logic

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -12,7 +12,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  user$: Observable<firebase.User|null>;
+  user$: Observable<firebase.User>;
   private authState: any;
   currentId: any;
 
@@ -24,10 +24,11 @@ export class AuthService {
     return this.user$;
   }
 
+  // email & password login
   login(email: string, password: string) {
     this.afAuth.signInWithEmailAndPassword(email, password)
       .then((value) => {
-        if (!value.user?.emailVerified) {
+        if (!value.user.emailVerified) {
           console.log('Please verify your email to login');
         }
         else {
@@ -37,5 +38,74 @@ export class AuthService {
       .catch((err: { message: any }) => {
         console.log('Something went wrong: ', err.message);
       });
+  }
+
+  emailSignup(displayName: string, email: string, password: string, confirmPassword: string) {
+    this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then(async (user: firebase.auth.UserCredential) => {
+        await firebase.auth().currentUser.sendEmailVerification();
+        this.authState = user;
+        const currentId = this.authState.user.uid;
+        const status = 'online';
+        this.setUserData(email, displayName, status, currentId);
+        this.router.navigateByUrl('/login');
+      })
+      .catch((error: any) => {
+        console.log('Something went wrong: ', error);
+      });
+  }
+
+  // move user data to real-time database
+  setUserData(email: string, displayName: string, status: string, currentId: any): void {
+    const path = `users/${currentId}`;
+    const data = {
+      email,
+      displayName,
+      status
+    };
+    this.db.object(path).update(data)
+      .catch(error => console.log(error));
+  }
+
+  // email signup/login
+  googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.oAuthLogin(provider)
+      .then((value: firebase.auth.UserCredential) => {
+        console.log('Success', value), this.router.navigateByUrl('/home');
+        const currentId = value.user.uid;
+        const status = 'online';
+        this.setUserData(value.user.email, value.user.displayName, status, currentId);
+      })
+      .catch((error: any) => {
+        console.log('Something went wrong: ', error);
+      });
+  }
+
+  // reset password logic
+  resetPassword(email: string) {
+    return firebase
+      .auth()
+      .sendPasswordResetEmail(email)
+      .then((value: any) => {
+        console.log('We have sent you a password reset link');
+        this.router.navigateByUrl('/login');
+      })
+      .catch((err: { message: any }) => {
+        console.log('Something went wrong: ', err.message);
+      });
+  }
+
+  // logout
+  logout() {
+    this.afAuth.signOut().then(() => {
+      console.log('signOut successful');
+      this.router.navigate(['/login']);
+      return 'You have been signed out.';
+    });
+  }
+
+  private oAuthLogin(provider: any) {
+    return this.afAuth.signInWithPopup(provider);
   }
 }
